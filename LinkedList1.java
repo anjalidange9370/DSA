30811c4674dd5f57e5ced9010fd2e6dde6b6882f
Anjali Dange <anjalidange6647@gmail.com>
2026-05-10 16:40:59 +0530

    Add files via upload

--- LinkedList1.java
+++ LinkedList1.java
@@ -1,12 +1,35 @@
-public class LinkedList1{
+    /*Linked List is a linear data structure.
+           Elements are stored in separate nodes.
+           Each node contains:
+          - Data
+          - Address of next node (next)
+          First node = Head
+          Last node points to null.
+      
+          Dynamic size (can grow/shrink at runtime)
+          Easy insertion and deletion
+          Memory is allocated when needed
+          No fixed size required
+
+          Extra memory needed for pointer/reference
+          Random access not possible
+          (cannot directly access like array index)
+          Traversing is slower than arrays
+    
+           Node-> Single element of linked list
+           Head -> Starting node
+           Tail      -> Last node
+           Traversal -> Visiting each node
+      */
+    public class LinkedList1{
     public static class Node{
         Node next;
-        int data;
+     int data;
         Node(int data){
-this.data=data;
-        }
+    this.data=data;
+        }   
     }
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         Node a=new Node(5);
         Node b=new Node(4);
         Node c=new Node(3);
@@ -40,11 +63,11 @@ this.data=data;
          }
         System.out.println();
         System.out.println("==========================");
-      //4th way to print LinkedList
-      temp=a;
-      while(temp!=null){
-      System.out.print(temp.data+"  ");
-      temp=temp.next;
+       //4th way to print LinkedList
+       temp=a;
+       while(temp!=null){
+       System.out.print(temp.data+"  ");
+       temp=temp.next;
          }
          }
          }
